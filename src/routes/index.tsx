4a1f75f8e8e22956aeb970cd967621caacd2a42e
Alexson Almeida <as3904527@gmail.com>
2025-02-01 20:31:23 -0300

    adding the forgot-password flow

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { PublicLayout } from "@/layout/public/public-layout";
 import { Navigate, Route, Routes as RoutesReactRouter } from "react-router";
 import { LoginPage } from "@/pages/public/login";
+import { ForgotPasswordPage } from "@/pages/public/forgot-password";
+import { CodeConfirmationPage } from "@/pages/public/forgot-password/code-confirmation";
+import { CreateNewPasswordPage } from "@/pages/public/forgot-password/create-new-password";
 
 export function Routes() {
   return (
@@ -8,6 +11,9 @@ export function Routes() {
       <Route path="/" element={<PublicLayout />}>
         <Route index element={<Navigate to="/login" replace />} />
         <Route index path="login" element={<LoginPage />} />
+        <Route index path="forgot-password" element={<ForgotPasswordPage/>}/>
+        <Route path="forgot-password/code-confirmation" element={<CodeConfirmationPage/>}/>
+        <Route path="forgot-password/create-new-password" element={<CreateNewPasswordPage/>} />
       </Route>
 
       <Route path="dashboard" element={<h1>Dashboard</h1>} />
