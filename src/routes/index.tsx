a32b6ee655222b06a6a306be3f9c65f72a3e1eb8
Matheus Leandro <matheusleandro33@gmail.com>
2025-02-11 12:14:58 -0300

    feat: add global toaster notification to the project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,27 @@
 import { PublicLayout } from "@/layout/public/public-layout";
+import { ExpenseProvider } from "@/context/ExpenseContext";
 import { Navigate, Route, Routes as RoutesReactRouter } from "react-router";
 import { LoginPage } from "@/pages/public/login";
 import { DashboardPage } from "@/pages/private/dashboard";
 import { PrivateLayout } from "@/layout/private/private-layout";
 import { SavingsPage } from "@/pages/private/savings";
+import { Toaster } from "@/components/ui/toaster";
 
 export function Routes() {
   return (
-    <RoutesReactRouter>
-      <Route path="/" element={<PublicLayout />}>
-        <Route index element={<Navigate to="/login" replace />} />
-        <Route index path="login" element={<LoginPage />} />
-      </Route>
+    <ExpenseProvider>
+      <Toaster/>
+      <RoutesReactRouter>
+        <Route path="/" element={<PublicLayout />}>
+          <Route index element={<Navigate to="/login" replace />} />
+          <Route index path="login" element={<LoginPage />} />
+        </Route>
 
-      <Route path="/" element={<PrivateLayout />}>
-        <Route path="dashboard" element={<DashboardPage />} />
-        <Route path="savings" element={<SavingsPage />} />
-      </Route>
-    </RoutesReactRouter>
+        <Route path="/" element={<PrivateLayout />}>
+          <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="savings" element={<SavingsPage />} />
+        </Route>
+      </RoutesReactRouter>
+    </ExpenseProvider>
   );
 }
